2aace9d1237a149909ee5e55c6b15fd54b2139c2
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-02-01 15:13:28 -0300

    commit initial

---
+++ src/Screens/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
