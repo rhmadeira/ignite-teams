d85d70b543cd013366ac926bf9451801169d5d42
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-04-15 20:36:52 -0300

    feat:

--- App.tsx
+++ App.tsx
@@ -1,33 +1,26 @@
-import { NewGroup } from "@screens/NewGroup";
-import { StatusBar } from "react-native";
-import theme from "./src/theme"; //Irei usar o tema somente nesse arquivo
-import { ThemeProvider } from "styled-components";
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
-import { Loading } from "@components/Loading";
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import { Loading } from '@components/Loading';
+
+import { Players } from '@screens/Players';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-  });
-
-  // if (!fontsLoaded) {
-  //   return <ActivityIndicator />;
-  // }
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent //para o statusbar ficar por cima do conteúdo
-      />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      <>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <Players/> : <Loading/> }
+      </>
     </ThemeProvider>
   );
 }
